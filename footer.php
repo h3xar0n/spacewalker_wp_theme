132eebee1a4049dbc416292f945a05f5ed30f699
h3xar0n <aron@glambyadelina.com>
2017-03-19 12:22:19 -0400

    Copied footer

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!-- Footer -->
+  <footer>
+    <div class="row">
+      <div class="col-xs-8 foot">
+        <div id="legal">&copy; A<span class="stripe">r</span>on <span class="stripe">E</span>idelman 2017</div>
+      </div>
+      <div class="col-xs-4 text-right">
+        <!-- GitHub -->
+        <a href="https://github.com/h3xar0n" target="_blank"><i class="fa fa-github"></i></a>
+        <!-- LinkedIn -->
+        <a href="https://www.linkedin.com/in/aroneidelman" target="_blank"><i class="fa fa-linkedin"></i></a>
+      </div>
+    </div>
+  </footer>
